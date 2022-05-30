a710faca5a84a17b36ef3ecb4c9d2d9480b6ebdb
Monir <munir10bm@gmail.com>
2022-05-30 12:37:16 -0400

    pennywise tool initial commit

---
+++ lib/pennywise_view_tool.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "pennywise_view_tool/version"
+require_relative "pennywise_view_tool/renderer"
+
+module PennywiseViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
